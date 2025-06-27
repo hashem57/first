5ece61b584344ae10b7baf79b7e6cfec20485aa0
hashem57 <hashemmohsen320@gmail.com>
2025-06-27 23:41:02 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,71 +1,64 @@
-// start validate function 
+//Start validate function
+function validate() {
+    var userName = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    var errorMessage = document.getElementById("errorMessage");
 
-function validate(){
+    // Style ErrorMessage 
+    errorMessage.style.padding = "10px";
+    errorMessage.style.backgroundColor = "red";
+    errorMessage.style.color = "#fff";
+    errorMessage.style.textAlign = "center";
+    errorMessage.style.transition = "all .5s ease-in";
+    errorMessage.style.marginBottom = "20px";
 
-var userName = document.getElementById("username").value;  
+    var text = "";
 
-var email = document.getElementById("email").value;
+    if (!userName || !email || !password || !confirmPassword) {
+        text = "Please Enter Your data";
+        errorMessage.innerHTML = text;
+        return false;
+    } else if (userName.length < 5 || userName.length > 15) {
+        text = "please Insert 5-15 character in username";
+        errorMessage.innerHTML = text;
+        return false;
+    }
 
-var password = document.getElementById("password").value;
+    else if (email.indexOf("@") === -1 || email.indexOf(".") === -1) {
+        text = "Please Enter Valid Email";
+        errorMessage.innerHTML = text;
+        return false;
+    } else if (password.length < 8) {
+        text = "please Insert at least 8 character in password";
+        errorMessage.innerHTML = text;
+        return false;
+    }
 
-var confirmPassword = document.getElementById("confirmpassword").value;
+    else if (password != confirmPassword) {
+        text = "please Matched password";
+        errorMessage.innerHTML = text;
+        return false;
+    }
+    else {
 
-var errorMessage = document.getElementById("errorMessage");
-
-// style error message 
-errorMessage.style.padding= "10px";
-errorMessage.style.backgroundColor= "red";
-errorMessage.style.color ="#fff";
-errorMessage.style.textAlign="center";
-errorMessage.style.transition= "all .5s ease-in";
-errorMessage.style.marginBottom="20px";
-
-
-
-var text = ""; 
-
-if( email=="" && userName=="" && password==""&& confirmPassword==""){
-
-    text= "please Insert Valid Data";
-    errorMessage.innerHTML =text;
-    return false;
-}
-else if(userName.length <5 || userName.length >15){
-    text ="please Insert 5-15 character in username";
-    errorMessage.innerHTML =text;
-    return false;
-}
-
-else if(email.indexOf("@")==-1){
-    text= "Please Enter Valid Email";
-    errorMessage.innerHTML =text;
-    return false;
-}else if (password.length<8){
-    text ="please Insert atleast 8 character in password";
-    errorMessage.innerHTML =text;
-    return false;
-}
-
-else if(password != confirmPassword){
-    text= "please Matched password";
-    errorMessage.innerHTML =text;
-    return false;
-}
-else{
-   
-    return true;
-}
+        return true;
+    }
 
 
-}
-
-
-
-
-// end validate function 
+    }
 
 
 
 
 
 
+
+
+
+
+
+
+
+//End validate function
